44e6db8abea2ef4de389fdac66c20d21a487edcc
Eliah Snakin <ilya@nikans.ru>
2016-04-25 06:10:47 +0300

    style

--- esmc/static/map.js
+++ esmc/static/map.js
@@ -1,58 +1,131 @@
-$(function(){
-  function initMap() {
-		var map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 10,
-			center: {lat: 55.7558, lng: 37.6173}
-		});
-		return map;
+var currentGeoposition = null;
+
+var initMap = function() {
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 10,
+		center: {lat: 55.7558, lng: 37.6173}
+	});
+
+	var geoloccontrol = new klokantech.GeolocationControl(map, 14);
+
+	if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      currentGeoposition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      console.log(currentGeoposition);
+
+      // infoWindow.setPosition(pos);
+      // infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
   }
 
-  var polygonDraw = function(polygon, map) {
-    var gm_polygon = new google.maps.Polygon({
-      paths: polygon,
-      strokeColor: '#FF0000',
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: '#FF0000',
-      fillOpacity: 0.35
-    });
-    gm_polygon.setMap(map);
+	return map;
+}
 
-//        console.log(gm_polygon);
-  };
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  // infoWindow.setPosition(pos);
+  // infoWindow.setContent(browserHasGeolocation ?
+  //                       'Error: The Geolocation service failed.' :
+  //                       'Error: Your browser doesn\'t support geolocation.');
+}
 
-  var odhGetGoogleMapsPolygon = function(odh) {
-    var polygon = JSON.parse(odh.geometry.geometry_json).coordinates[0];
+var polygonDraw = function(polygon, map) {
+  var gm_polygon = new google.maps.Polygon({
+    paths: polygon,
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: '#FF0000',
+    fillOpacity: 0.35
+  });
+  gm_polygon.setMap(map);
 
-    var gm_polygon = polygon.map(function(ring) {
-      return new google.maps.MVCArray(
-        ring.map(function(coords){
-          return new google.maps.LatLng(coords[1], coords[0]);
-        })
-      );
-    });
+  // console.log(gm_polygon);
+};
 
-    return gm_polygon;
-  };
+var odhGetFirstCoordinate = function(odh) {
+	var coordinate = JSON.parse(odh.geometry.geometry_json).coordinates[0][0][0];
+	return new google.maps.LatLng(coordinate[1], coordinate[0]);
+}
 
-  var odhDraw = function(id, map) {
-    $.get('http://odh.esmc.info/api/v1/odh/'+id, function( data ) {
-      var gm_polygon = odhGetGoogleMapsPolygon(data);
+var odhGetGoogleMapsPolygon = function(odh) {
+  var polygon = JSON.parse(odh.geometry.geometry_json).coordinates[0];
+
+  // console.log(polygon);
+
+  var gm_polygon = polygon.map(function(ring) {
+    return new google.maps.MVCArray(
+      ring.map(function(coords){
+        return new google.maps.LatLng(coords[1], coords[0]);
+      })
+    );
+  });
+
+  return gm_polygon;
+};
+
+var odhDraw = function(id, map) {
+  $.get('http://odh.esmc.info/api/v1/odh/'+id, function( data ) {
+    var gm_polygon = odhGetGoogleMapsPolygon(data);
+    polygonDraw(gm_polygon, map);
+  });
+};
+
+var odhDrawPage = function(page, map) {
+  $.get('http://odh.esmc.info/api/v1/odh?page='+page, function( data ) {
+//          console.log(data.data);
+    data.data.forEach(function(odh){
+      var gm_polygon = odhGetGoogleMapsPolygon(odh);
       polygonDraw(gm_polygon, map);
     });
-  };
+  });
+};
 
-  var odhDrawPage = function(page, map) {
-    $.get('http://odh.esmc.info/api/v1/odh?page='+page, function( data ) {
-//          console.log(data.data);
-      data.data.forEach(function(odh){
-        var gm_polygon = odhGetGoogleMapsPolygon(odh);
-        polygonDraw(gm_polygon, map);
-      });
-    });
-  };
+var moveToCoordinate = function(lat, lng){
+  var center = new google.maps.LatLng(lat, lng);
+  map.panTo(center);
+  map.setZoom(14);	
+}
 
-  var map = initMap();
+var moveToLocation = function(latLng){
+  map.panTo(latLng);
+  map.setZoom(14);
+}
+
+var setBoundsToFitWithGeoposition = function(latLngArr) {
+	if(currentGeoposition != null && currentGeoposition !== undefined) {
+		latLngArr.push(currentGeoposition)
+	}
+	setBounds(latLngArr);
+}
+
+var setBounds = function(latLngArr) {
+
+		console.log(latLngArr);
+
+    var bounds = new google.maps.LatLngBounds();
+    for (var i=0; i < latLngArr.length; i++) {
+        bounds.extend(latLngArr[i]);
+    }
+    map.fitBounds(bounds);
+}
+
+
+var map = initMap();
+
+
+$(function(){
+  
   // for(i=0; i<500; i++) {
   //   odhDrawPage(i, map);
   // }
--- esmc/static/task.js
+++ esmc/static/task.js
@@ -1,68 +1,143 @@
-$(function(){
-	var activeTaskId = null;
+var startup = true
+var activeTaskId = null;
 
-	var checkActiveTask = function() {
+var checkActiveTask = function() {
 
-		if(activeTaskId == null) {
-			updateDOMforFetching()
-		}
+	// console.log(activeTaskId);
 
-		$.get('/ajax', function( data ) {
-			data = data.slice(-1)[0];
-      updateDOMforTaskContainer(data.pk, data.fields);
-    })
-    .fail(function() {
-    	updateDOMforError()
-    	console.log('shit')
-  	});
-	};
-
-	var updateDOMforFetching = function() {
-		$('#taskNotask').hide();
-		$('#taskActive').hide();
-		$('#taskFetchError').hide();
-		$('#taskFetching').show();
+	if(startup === true) {
+		updateDOMforFetching()
 	}
 
-	var updateDOMforError = function() {
-		$('#taskNotask').hide();
-		$('#taskActive').hide();
-		$('#taskFetchError').show();
-		$('#taskFetching').hide();
-	}
+	$.get('/ajax/', function( data ) {
+		startup = false
 
-	var updateDOMforTaskContainer = function(taskId, data) {
-		if(taskId == null) {
-			$('#taskNotask').show();
-			$('#taskActive').hide();
+		if(data.length == 0) {
+			updateDOMforTaskContainer(null, null);
+			return;
 		}
-		else if(taskId != activeTaskId) {
+
+		data = data.slice(-1)[0];
+    updateDOMforTaskContainer(data.pk, data.fields);
+    // console.log(data.fields)
+  })
+  .fail(function() {
+  	updateDOMforError()
+	});
+};
+
+var updateDOMforFetching = function() {
+	$('#taskNotask').hide();
+	$('#taskActive').hide();
+	$('#taskFetchError').hide();
+	$('#taskFetching').show();
+}
+
+var updateDOMforError = function() {
+	$('#taskNotask').hide();
+	$('#taskActive').hide();
+	$('#taskFetchError').show();
+	$('#taskFetching').hide();
+}
+
+var updateDOMforTaskContainer = function(taskId, data) {
+	if(taskId == null) {
+		$('#taskNotask').show();
+		$('#taskActive').hide();
+	}
+	else if(taskId != activeTaskId) {
+		if(data.status == 'new') {
 			$('#newTaskModal').modal();
-			$('#taskNotask').hide();
-			$('#taskActive').show();
-			activeTaskId = taskId;
-			updateDOMforActiveTask(data)
 		}
-		$('#taskFetchError').hide();
-		$('#taskFetching').hide();
+		$('#taskNotask').hide();
+		$('#taskActive').show();
+		activeTaskId = taskId;
+		updateDOMforActiveTask(data)
 	}
+	$('#taskFetchError').hide();
+	$('#taskFetching').hide();
+}
 
-	var updateDOMforActiveTask = function(data) {
-		$('#taskRouteName').text(data);
-		$('#taskTime').text(data);
-		$('#taskTechOp').text(data);
-		$('#taskOdhList');
-		$('#taskComment').text(data);
-		$('#taskRouteName').text(data);
+var getOdh = function(id, move) {
+	$.get('http://odh.esmc.info/api/v1/odh/'+id, function( data ) {
+		// console.log(data);
+		$('#taskOdhList').append('<li>'+data.name+'</li>');
+
+    var gm_polygon = odhGetGoogleMapsPolygon(data);
+    polygonDraw(gm_polygon, map);
+
+    if(move == true) {
+    	// console.log(odhGetFirstCoordinate(data))
+    	// moveToLocation(odhGetFirstCoordinate(data))
+
+    	setBoundsToFitWithGeoposition([odhGetFirstCoordinate(data)]);
+    }
+  });
+}
+
+var updateDOMforActiveTask = function(data) {
+	$('#taskRouteName, #newTaskRouteName').text(data.route[0]);
+	moment().locale('ru');
+	var dateStart = moment(new Date(data.dateStart));
+	var dateFinish = moment(new Date(data.dateFinish));
+	var dateString = dateStart.locale('ru').format('DD.MM hh:mm') + " — " + dateFinish.locale('ru').format('hh:mm');
+	$('#taskDate, #newTaskDate').text(dateString);
+	$('#taskTechOp, #newTaskTechOp').text(data.techOp);
+	$('#taskOdhList').empty()
+	var odhIds = data.route[1].split(",");
+	if(odhIds.length > 0) {
+		odhIds.forEach(function(odhId, i) {
+			getOdh(odhId, i==0)
+		});
 	}
+	$('#taskOdhList');
+	$('#taskComment, #newTaskComment').text(data.comment);
+}
 
-	setTimeout(function() {
+var changeStatus = function(id, status) {
+  console.log(status);
+	$.post('/set_status/', { 'id': id, 'status': status }, function( data ) {
+    // console.log(data);
+    // $('#newTaskModal').modal('hide');
+    if(status == 'completed') {
+    	activeTaskId = null;
+    }
+    checkActiveTask();
+  });
+}
+
+var updateMapHeight = function() {
+	var height = Math.max($(document).height(), $(window).height()) - $('#navbar').height();
+	$('#map').height(height);
+	$('#taskActive').height(height);
+}
+
+
+$(function(){
+
+	setInterval(function() {
 		checkActiveTask();
-	}, 2000);
+	}, 5000);
 
 	$('#taskCompletedButton').on('click', function(e) {
-		$.post('', data, function( data ) {
-      
-    });
+		// console.log('comp');
+		if(activeTaskId == null) return;
+		changeStatus(activeTaskId, 'completed');
+	});
+
+	$(window).on('resize', function(){
+    updateMapHeight()
+  }).trigger('resize');
+	
+
+	// $('#newTaskAcceptButton').on('click', function(e) {
+	// 	// console.log('acc');
+	// 	if(activeTaskId == null) return;
+	// 	changeStatus(activeTaskId, 'accepted');
+	// });
+
+	$('#newTaskModal').on('hide.bs.modal', function (e) {
+  	changeStatus(activeTaskId, 'accepted');
 	})
+
 });
